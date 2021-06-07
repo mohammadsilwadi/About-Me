d5706ac196bc444d7775db52d6f48127e6442d6f
mohammadsilwadi <mohammadsilwadi1@gmail.com>
2021-06-07 16:15:23 +0300

    alert change

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
 'use strict';
 let userName= prompt('what is your name')
-alert('wlecome '+userName+ ' to my gussing game')
+alert('wlecome '+userName+ ' to my guessing game')
 
 let like= prompt('do you think I like sport?').toLowerCase();
 if (like === 'yes' || like === 'y'){
     
     //console.log(like)
-    alert('great')
+    alert(like)
 
 }else if (like === 'no' || like === 'n'){
 
-    //console.log(like)
+    
     alert('I love sport')
 }else {
     alert('Please type (yes) or (no) or (y) or (n)')
@@ -21,11 +21,11 @@ let hate= prompt('do you think I hate cooking?').toLowerCase();
 if (hate === 'yes' || hate === 'y'){
     
     //console.log(hate)
-    alert('great')
+    alert(hate)
 
 }else if (hate === 'no' || hate === 'n'){
 
-    //console.log(hate)
+    
     alert('I hate cooking')
 }else {
     alert('Please type (yes) or (no) or (y) or (n)')
@@ -34,13 +34,12 @@ if (hate === 'yes' || hate === 'y'){
 let live= prompt('do you think I live in PARIS ?').toLowerCase();
 if (live === 'yes' || live === 'y'){
     
-    //console.log(live)
     alert('I live in Amman')
 
 }else if (live === 'no' || live === 'n'){
 
     //console.log(live)
-    alert('I live in Amman')
+    alert(live)
 }else {
     alert('Please type (yes) or (no) or (y) or (n)')
 
@@ -48,13 +47,13 @@ if (live === 'yes' || live === 'y'){
 let age= prompt('do you think I have 50 years old ?').toLowerCase();
 if (age === 'yes' || age === 'y'){
     
-    //console.log(age)
+    
     alert('I am 24 years old')
 
 }else if (age === 'no' || age === 'n'){
 
     //console.log(age)
-    alert('I am 24 years old')
+    alert(age)
 }else {
     alert('Please type (yes) or (no) or (y) or (n)')
 
@@ -64,13 +63,13 @@ let luckyNumber= prompt('IS 159 is my lucky number').toLowerCase();
 
 if (luckyNumber === 'yes' || luckyNumber === 'y'){
     
-    //console.log(luckyNumber)
+    
     alert('my lucky Number is 7')
 
 }else if (luckyNumber === 'no' || luckyNumber === 'n'){
 
     //console.log(luckyNumber)
-    alert('my lucky Number is 7')
+    alert(luckyNumber)
 }else {
     alert('Please type (yes) or (no) or (y) or (n)')
 
