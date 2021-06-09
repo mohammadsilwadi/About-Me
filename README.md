0990be4b91856f32503609dbb9a6c14113f8d699
mohammadsilwadi <mohammadsilwadi1@gmail.com>
2021-06-09 17:44:58 +0300

    add function to the code

--- README.md
+++ README.md
@@ -3,5 +3,24 @@
 this website contant guessing game
 add q6 + q7 
 I used for loop  and make  list in html in tow ways order list and unordered list .
+mohammad silwadi was Navigator on this about me project.
+ ahmad smadi was Driver on this about me project.
+### this function added by ahmad smadi and mohammad silwadi
+function fiveQuestion (question , correct , uncorrect){
+let userInput; 
+do {
+  userInput= prompt(question).toLocaleLowerCase();
 
+} while (userInput!== 'yes' && userInput !== 'y' && userInput !== 'no' && userInput !== 'n')
 
+if( userInput === 'yes' || userInput === 'y') {
+console.log (correct);
+} else {
+  console.log(uncorrect);
+}
+}
+fiveQuestion('do you think I like sport?', 'you are right','wrong' )
+fiveQuestion('do you think I hate cooking?', 'you are right','wrong' )
+fiveQuestion('do you think I live in PARIS?', 'you are right','wrong' )
+fiveQuestion('do you think I have 50 years old?', 'you are right','wrong' )
+fiveQuestion('Is my  GPA in university  3.8?', 'you are right','wrong' ) .
--- js/app.js
+++ js/app.js
@@ -1,127 +1,11 @@
 'use strict';
 
-// let rightAnswer=0;
-// let userName= prompt('what is your name');
-// alert('wlecome '+userName+ ' to my guessing game');
-
-// let like= prompt('do you think I like sport?').toLowerCase();
-// if (like === 'yes' || like === 'y'){
-
-//   //console.log( like )
-//   alert( 'correct' );
-//   rightAnswer++;
-
-// }else if (like === 'no' || like === 'n'){
+let rightAnswer=0;
+let userName= prompt('what is your name');
+alert('wlecome '+userName+ ' to my guessing game');
 
 
-//   alert('I love sport');
-// }else {
-//   alert('Please type (yes) or (no) or (y) or (n)');
-
-// }
-// let hate= prompt('do you think I hate cooking?').toLowerCase();
-
-// if (hate === 'yes' || hate === 'y'){
-
-//   //console.log(hate)
-//   alert('correct');
-//   rightAnswer++;
-// }else if (hate === 'no' || hate === 'n'){
-
-
-//   alert('I hate cooking');
-// }else {
-//   alert('Please type (yes) or (no) or (y) or (n)');
-
-// }
-// let live= prompt('do you think I live in PARIS ?').toLowerCase();
-// if (live === 'yes' || live === 'y'){
-
-//   alert('I live in Amman');
-
-// }else if (live === 'no' || live === 'n'){
-
-//   //console.log(live)
-//   alert('correct');
-//   rightAnswer++;
-// }else {
-//   alert('Please type (yes) or (no) or (y) or (n)');
-
-// }
-// let age= prompt('do you think I have 50 years old ?').toLowerCase();
-// if (age === 'yes' || age === 'y'){
-
-
-//   alert('I am 24 years old');
-
-// }else if (age === 'no' || age === 'n'){
-
-//   //console.log(age)
-//   alert('correct');
-//   rightAnswer++;
-// }else {
-//   alert('Please type (yes) or (no) or (y) or (n)');
-
-// }
-// let gba= prompt('Is my  GPA in university  3.8').toLowerCase();
-// if (gba === 'yes' || gba === 'y'){
-
-//   alert('my gba 2.95');
-
-// }else if (gba === 'no' || gba === 'n'){
-
-//   //console.log(luckyNumber)
-//   alert('correct');
-//   rightAnswer++;
-// }else {
-//   alert('Please type (yes) or (no) or (y) or (n)');}
-// let guessNumber= 0;
-// for(let i=0; i<4;i++){
-//   guessNumber=prompt('guess my lucky number');
-//   guessNumber=parseInt(guessNumber);
-//   if (guessNumber === 7){
-//     rightAnswer++;
-//     //console.log( guessNumber )
-//     break;
-//   }
-//   else if (guessNumber >=8 && guessNumber<=11){
-//     alert('too close');
-//   }
-//   else if (guessNumber >=3&&guessNumber<=6 ){
-//     alert('too close');
-//   }
-//   else if(12 <= guessNumber ){
-//     alert('too high from the answer');
-//   }
-//   else if (guessNumber <= 2 ){
-//     alert('too low from the answer');
-//   }
-//   else alert('only numbers allowed!!');
-
-
-
-// }
-
-
-
-// let place =['italy','france','madrid','uae','morocco','egypt'];
-// for(let b =0; b<6;b++){
-//   let visit=prompt('which country I am thinking to visit').toLowerCase();
-
-//   if (visit === place[0] || visit ===place[1] || visit ===place[2] ||visit ===place[3] ||visit ===place[4] ||visit ===place[5] ){
-//     // eslint-disable-next-line no-unused-vars
-//     rightAnswer++;
-//     //console.log( visit )
-//     break;
-//   } else (visit !== place[0] || visit !==place[1] || visit !==place[2] ||visit !==place[3] ||visit !==place[4] ||visit !==place[5] );{
-//     alert(' hint :country in europe or africa');
-//   }
-// }
-// alert(' these are the countries that i am thinking to visit italy,france,madrid,uae,morocco,egypt');
-// alert('you answer a ' + rightAnswer + ' questions right from seven questions') ;
-// alert('see you soon ' + userName) ;
-
-function fiveQuestion (question , correct , uncorrect){
+function trueQuestion (question , correct , uncorrect){
 let userInput; 
 do {
   userInput= prompt(question).toLocaleLowerCase();
@@ -129,13 +13,78 @@ do {
 } while (userInput!== 'yes' && userInput !== 'y' && userInput !== 'no' && userInput !== 'n')
 
 if( userInput === 'yes' || userInput === 'y') {
-console.log (correct);
+// console.log (correct)
+alert(correct)
+rightAnswer++
+;
 } else {
-  console.log(uncorrect);
+  alert(uncorrect);
 }
 }
-fiveQuestion('do you think I like sport?', 'you are right','wrong' )
-fiveQuestion('do you think I hate cooking?', 'you are right','wrong' )
-fiveQuestion('do you think I live in PARIS?', 'you are right','wrong' )
-fiveQuestion('do you think I have 50 years old?', 'you are right','wrong' )
-fiveQuestion('Is my  GPA in university  3.8?', 'you are right','wrong' )
+trueQuestion('do you think I like sport?', 'you are right','I love sport' );
+trueQuestion('do you think I hate cooking?', 'you are right','I hate cooking' );
+function falseQuestion (question , correct , uncorrect){
+  let userInput; 
+  do {
+    userInput= prompt(question).toLocaleLowerCase();
+  
+  } while (userInput!== 'yes' && userInput !== 'y' && userInput !== 'no' && userInput !== 'n')
+  
+  if( userInput === 'yes' || userInput === 'y') {
+alert(correct)
+  ;
+  } else {
+    //console.log(uncorrect);
+    alert(uncorrect)
+    rightAnswer++
+  }
+  }
+
+  falseQuestion('Is my  GPA in university  3.8' ,'my GPA is 2.95','you are right');
+  falseQuestion('do you think I live in PARIS ?' , 'I live in amman' , 'you are right');
+  falseQuestion('do you think I have 50 years old ?' , 'I am 24 years old' , 'you are right');
+function question5(){
+  let guessNumber= 0;
+for(let i=0; i<4;i++){
+  guessNumber=prompt('guess my lucky number');
+  guessNumber=parseInt(guessNumber);
+  if (guessNumber === 7){
+    rightAnswer++;
+    //console.log( guessNumber )
+    break;
+  }
+  else if (guessNumber >=8 && guessNumber<=11){
+    alert('too close');
+  }
+  else if (guessNumber >=3&&guessNumber<=6 ){
+    alert('too close');
+  }
+  else if(12 <= guessNumber ){
+    alert('too high from the answer');
+  }
+  else if (guessNumber <= 2 ){
+    alert('too low from the answer');
+  }
+  else alert('only numbers allowed!!');
+
+}}
+question5();
+
+function question6(){
+let place =['italy','france','madrid','uae','morocco','egypt'];
+for(let b =0; b<6;b++){
+  let visit=prompt('which country I am thinking to visit').toLowerCase();
+
+  if (visit === place[0] || visit ===place[1] || visit ===place[2] ||visit ===place[3] ||visit ===place[4] ||visit ===place[5] ){
+    // eslint-disable-next-line no-unused-vars
+    rightAnswer++;
+    //console.log( visit )
+    break;
+  } else (visit !== place[0] || visit !==place[1] || visit !==place[2] ||visit !==place[3] ||visit !==place[4] ||visit !==place[5] );{
+    alert(' hint :country in europe or africa');
+  }
+}}
+question6();
+alert(' these are the countries that i am thinking to visit italy,france,madrid,uae,morocco,egypt');
+alert('you answer a ' + rightAnswer + ' questions right from seven questions') ;
+alert('see you soon ' + userName) ;
